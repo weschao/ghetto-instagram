d321b8905da01256349e63cddc46c9a793bd61ae
Wes Chao <weschao@fb.com>
2014-10-15 13:08:04 -0700

    Initial Commit

---
+++ Ghetto Instagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ghetto Instagram
+//
+//  Created by Wes Chao on 10/15/14.
+//  Copyright (c) 2014 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
